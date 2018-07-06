9b34cc6cd7cc8fa496706e5452057a5a37871e00
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-07-06 12:33:10 -0500

    finished most other things just need to make the main mechanic work and add comparing the games Array to he users Array

--- js/main.js
+++ js/main.js
@@ -2,45 +2,94 @@
     "use strict";
 
     // Assigning buttons to variables
-    var blueButton = $('.bluebutton');
-    var greenButton = $('.greenbutton');
-    var redButton = $('.redbutton');
-    var yellowButton = $('.yellowbutton');
+    var colorFadeDuration = 200;
     var delayDuration = 0;
+    var userArr = [];
+    var gameArr = [];
 
-    // Randomly choose 1 of the 4 buttons
-    var randomNumber = Math.floor((Math.random() * 4) + 1);
-    console.log(randomNumber);
-    switch (randomNumber) {
-        case 1:
-            flashRedButton();
-            break;
-        case 2:
-            flashBlueButton();
-            break;
-        case 3:
-            flashGreenButton();
-            break;
-        case 4:
-            flashYellowButton();
-            break;
-        default:
-            console.log('NUMBER WAS NOT 1-4');
+
+    function grabRandomNumber() {
+        // Randomly choose 1 of the 4 buttons
+        var randomNumber = Math.floor((Math.random() * 4) + 1);
+
+        // Uses randomNumber to choose
+        switch (randomNumber) {
+            case 1:
+                flashRedButton(gameArr);
+                break;
+            case 2:
+                flashBlueButton(gameArr);
+                break;
+            case 3:
+                flashGreenButton(gameArr);
+                break;
+            case 4:
+                flashYellowButton(gameArr);
+                break;
+            default:
+                console.log('NUMBER WAS NOT 1-4');
+        }
     }
 
-    function flashBlueButton() {
+    function flashBlueButton(arr) {
+        $('#blueButton').animate({backgroundColor: "#77F"},
+            colorFadeDuration).delay(delayDuration).animate({backgroundColor: "#00F"}, colorFadeDuration);
+        return arr.push('blue');
     }
 
-    function flashGreenButton() {
+    function flashGreenButton(arr) {
+        $('#greenButton').animate({backgroundColor: "#7F7"},
+            colorFadeDuration).delay(delayDuration).animate({backgroundColor: "#0F0"}, colorFadeDuration);
+        return arr.push('green');
     }
 
-    $('#redButton').animate({backgroundColor:"#F77"}, 200).delay(delayDuration).animate({backgroundColor:"red"}, 200);
-
-    function flashRedButton() {
+    function flashRedButton(arr) {
         console.log("red button pressed");
+        $('#redButton').animate({backgroundColor: "#F77"},
+            colorFadeDuration).delay(delayDuration).animate({backgroundColor: "#F00"}, colorFadeDuration);
+        return arr.push('red');
     }
 
-    function flashYellowButton() {
+    function flashYellowButton(arr) {
+        $('#yellowButton').animate({backgroundColor: "#FF7"},
+            colorFadeDuration).delay(delayDuration).animate({backgroundColor: "#DD0"}, colorFadeDuration);
+        return arr.push('yellow');
+
     }
 
-})();
+    $('#blueButton').click(function () {
+        flashBlueButton(userArr);
+        console.log(userArr);
+    });
+    $('#greenButton').click(function () {
+        flashGreenButton(userArr);
+        console.log(userArr);
+    });
+    $('#redButton').click(function () {
+        flashRedButton(userArr);
+        console.log(userArr);
+    });
+    $('#yellowButton').click(function () {
+        flashYellowButton(userArr);
+        console.log(userArr);
+    });
+
+    function createGameArray(roundNumber) {
+        for (var i = 0; i > roundNumber; i++) {
+            grabRandomNumber().delay(800);
+        }
+    }
+
+    function playRound(roundNumber) {
+        console.log("setting up rounds");
+        while (roundNumber < 10) {
+            console.log("starting round");
+            createGameArray(roundNumber);
+            console.log("finishing Round");
+            roundNumber++
+        }
+    }
+
+    $('startButton').click(playRound(1));
+})
+();
